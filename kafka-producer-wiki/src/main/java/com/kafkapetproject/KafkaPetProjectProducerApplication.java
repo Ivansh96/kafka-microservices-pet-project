4ddf73c69653f5ebd2c0dfea2ef8e86bcdc06c10
Ivansh96 <evgenijshavshin@yandex.ru>
2023-05-26 16:30:09 +0700

    parent project and child module(producer)

---
+++ kafka-producer-wiki/src/main/java/com/kafkapetproject/KafkaPetProjectProducerApplication.java
@@ -0,0 +1,12 @@
+package com.kafkapetproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPetProjectProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaPetProjectProducerApplication.class);
+    }
+}
---
+++ src/test/java/com/kafkapetproject/KafkaPetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkapetproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
